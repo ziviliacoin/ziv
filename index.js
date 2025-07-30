954b223cc1492a5b8a2c24f3489f935dd59185d4
ziviliacoin <ceb492134@gmail.com>
2025-07-30 14:13:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+// Route utama
 app.get('/', (req, res) => {
-  res.send('Bot is running!');
+  res.send('ZIV wallet backend is alive! 🚀');
 });
 
+// Port listener (Railway akan auto-set PORT environment)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is live on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
